81d79548b32056e4d5c61a016cfe2852f90e34d9
Deependra Mishra <itisdeep@gmail.com>
2024-02-04 20:13:04 +0530

    changing output

--- aws-network/outputs.tf
+++ aws-network/outputs.tf
@@ -1,9 +1,9 @@
-output "private_subnet_cidr" {
-    value = aws_subnet.private_subnet[*].cidr_block
+output "private_subnet_id" {
+    value = aws_subnet.private_subnet[*].id
 }
 
-output "public_subnet_cidr" {
-    value = aws_subnet.public_subnet[*].cidr_block
+output "public_subnet_id" {
+    value = aws_subnet.public_subnet[*].id
 }
 
 output "nat_eips" {
