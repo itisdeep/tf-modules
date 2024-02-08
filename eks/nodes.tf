e8ed8ae96accd96da4fc82bf78a9afeda1e8f5b2
Deependra Mishra <itisdeep@gmail.com>
2024-02-08 07:00:43 +0530

    fixed subnet error for nodes

--- eks/nodes.tf
+++ eks/nodes.tf
@@ -33,7 +33,7 @@ resource "aws_eks_node_group" "private-nodes" {
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.nodes.arn
 
-  subnet_ids = [data.aws_subnets.private.id]
+  subnet_ids = [for subnet in data.aws_subnet.pvt: subnet.id]
 
   capacity_type  = "ON_DEMAND"
   instance_types = [var.instance_type]
